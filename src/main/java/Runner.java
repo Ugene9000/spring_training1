16623a80e365127f2c3a4fc5320ddce599920606
Eugene <eugene.zubritski@gmail.com>
2019-04-27 23:06:47 +0300

    replaced Classpathappcontext with FileSystemxmlappcontext

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,17 +1,18 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/beans/beans.xml");
 
         Person person = (Person) context.getBean("person");
         person.speak();
 
         System.out.println(person.toString());
 
-        ((ClassPathXmlApplicationContext)context).close();
+        ((FileSystemXmlApplicationContext)context).close();
     }
 }
